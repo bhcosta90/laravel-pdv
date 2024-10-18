dfa204fd1f02220960a9a4576f624fdf78d39992
Bruno Costa <bhcosta90@gmail.com>
2024-10-18 07:28:10 -0300

    add daisy ui

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,11 @@
 
 declare(strict_types = 1);
 
+use App\Livewire;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('counter', Livewire\Counter::class);
